b05b19651c31df6c08beef33d561750efb6c8773
Ed Knutson <knutsoned@gmail.com>
2012-06-20 19:02:18 -0400

    upgrade to 2.4.1

--- oc-content/themes/modern/footer.php
+++ oc-content/themes/modern/footer.php
@@ -18,9 +18,11 @@
      *      You should have received a copy of the GNU Affero General Public
      * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
      */
-?>
 
-<?php osc_show_widgets('footer'); ?>
+    osc_show_widgets('footer');
+    $sQuery = osc_esc_js(__("ie. PHP Programmer", 'modern'));
+?>
+<!-- footer -->
 <div id="footer">
     <div class="inner">
         <a href="<?php echo osc_contact_url(); ?>"><?php _e('Contact', 'modern') ; ?></a> |
@@ -28,6 +30,21 @@
         <?php while( osc_has_static_pages() ) { ?>
             <a href="<?php echo osc_static_page_url() ; ?>"><?php echo osc_static_page_title() ; ?></a> |
         <?php } ?>
-        <?php _e('This website is proudly using the <a title="OSClass web" href="http://osclass.org/">open source classifieds</a> software <strong>OSClass</strong>', 'modern'); ?>.
+        <?php _e('This website is proudly using the <a title="OSClass web" href="http://osclass.org/">classifieds scripts</a> software <strong>OSClass</strong>', 'modern') ; ?>
     </div>
 </div>
+<!-- /footer -->
+</div>
+<!-- /container -->
+<script type="text/javascript">
+    var sQuery = '<?php echo $sQuery ; ?>' ;
+    function doSearch() {
+        if($('input[name=sPattern]').val() == sQuery || $('input[name=sPattern]').val().length < 3 || $('input[name=sPattern]').val() == ''){
+            $('input[name=sPattern]').css('background', '#FFC6C6');
+            $('#search-example').text('<?php echo osc_esc_js( __('Your search must be at least three characters long','avisame') ) ; ?>')
+            return false;
+        }
+        return true;
+    }
+</script>
+<?php osc_run_hook('footer') ; ?>
